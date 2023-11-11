4f4b50ab63d1d3391f2b462ee9192c6208a5a853
Shabnaj L <shabnajliaqat@gmail.com>
2023-11-11 14:33:33 +0600

    Create Two Sum.cpp
    https://leetcode.com/problems/two-sum/submissions/

---
+++ Two Sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> nums, int target) {
+        int sum = 0;
+        for(int i=0;i<(nums.size()-1);i++){
+            for(int j=1+i; j<nums.size();j++){
+                sum = nums[i] + nums[j];
+                //cout<<sum;
+                if(sum == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
