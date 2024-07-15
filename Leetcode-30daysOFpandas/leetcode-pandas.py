487a7eb9e2e3edd391fe56fd86965e86cbc276ab
Shabnaj L <shabnajliaqat@gmail.com>
2024-07-15 21:42:42 +0600

    Create leetcode-pandas.py

---
+++ Leetcode-30daysOFpandas/leetcode-pandas.py
@@ -0,0 +1,15 @@
+### 595. Big Countries
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+        big_countries = world[ (world['area']>=3000000) | (world['population']>=25000000)]
+        return big_countries[['name', 'population', 'area']]
+
+### 1757. Recyclable and Low Fat Products
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    found = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+    return found[['product_id']]
+
+### 
